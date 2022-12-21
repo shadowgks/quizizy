96d13d967c83b3edaf7df990cd705025996f12ae
shadowgks <saad.moumou.coder@gmail.com>
2022-12-21 17:29:44 +0100

    get info json

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 // Get elements
 const btn_start    = document.querySelector(".btn_start button");
 const box_info     = document.querySelector(".box_info");
-const btn_end      = document.querySelector(".quit");
-const btn_continue = document.querySelector(".continue");
+const btn_end      = document.querySelectorAll("button.quit");
+const btn_continue = document.querySelectorAll("button.continue");
 const box_user     = document.querySelector(".box_user");
+const box_resultat = document.querySelector(".box_resultat");
+const box_quiz     = document.querySelector(".box_quiz");
 
 //if clicked btn start Quiz
 btn_start.addEventListener('click',()=>{
@@ -12,14 +14,41 @@ btn_start.addEventListener('click',()=>{
 });
 
 //if clicked btn end Quiz
-btn_end.addEventListener('click',()=>{
+btn_end[0].addEventListener('click',()=>{
     box_info.style.display = 'none';
     btn_start.style.display = 'block';
+});
+btn_end[1].addEventListener('click',()=>{
     box_user.style.display = 'none';
+    btn_start.style.display = 'block';
+});
+btn_end[2].addEventListener('click',()=>{
+    box_resultat.style.display = 'none';
+    btn_start.style.display = 'block';
 });
 
+
 //if clicked btn continue Quiz
-btn_continue.addEventListener('click',()=>{
+btn_continue[0].addEventListener('click',()=>{
     box_info.style.display = 'none';
     box_user.style.display = 'block';
 });
+btn_continue[1].addEventListener('click',()=>{
+    box_user.style.display = 'none';
+    box_quiz.style.display = 'block';
+});
+
+// Get Info JSON
+function Show(){
+    let myRequest = new XMLHttpRequest();
+
+    myRequest.onreadystatechange = () => {
+        if(this.readyState === 4 && this.readyState === 200){
+            console.log(this.responseText);
+        }
+    }
+
+    myRequest.open("GET","assets/js/quizizy.json",true);
+    myRequest.send();
+}
+Show();
