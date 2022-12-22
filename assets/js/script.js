978a753561ffbdd39ecc1daaa193e85e71f85df1
shadowgks <saad.moumou.coder@gmail.com>
2022-12-22 20:31:29 +0100

    fix fun questionSelected

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,53 +47,31 @@ btn_continue[1].addEventListener('click',()=>{
 
 //if clicked btn next question
 let next_count = 0;
-let count_span = 1;
+let count_question = 1;
 btn_next.addEventListener('click',()=>{
     if(next_count < 9){  //ineed fix this
         next_count++;
-        count_span++;
-        showQuestions(next_count);
+        count_question++;
+        showQuestions();
     }else{
         console.log("good");
     }
 })
-// count question
-function countQuestions(n,lengthdata){
-    const count_question_step = document.querySelector(".quiz_footer .step");
-    const count_sum_question  = document.querySelector(".quiz_footer .all");
-    count_question_step.innerHTML = n;
-    count_sum_question.innerHTML  = lengthdata;
-};
 
-function showQuestions(index){
+function showQuestions(){
     // return new Promise((resolve,reject)=>{
-           // Get Info JSON
+    // Get Info JSON
     let myRequest = new XMLHttpRequest();
     myRequest.onreadystatechange = function(){
         if(this.readyState === 4 && this.status === 200){
             //Convert To Object JS
-            let objJS = JSON.parse(this.responseText);
-            console.log(objJS);
-            console.log(objJS[0].id );
+            let obj_js = JSON.parse(this.responseText);
             // resolve(objJS)
-            // //testquiziz
-            // console.log(objJS[0].question);
 
-            //Select box quiz
-            const quiz_title         = document.querySelector('.box_quiz .title');
-            const quiz_questions     = document.querySelector('.box_quiz .questions');
-            quiz_title.innerHTML     = '<h2>'+ objJS[index].id +'. '+objJS[index].question +'</h2>';
-           quiz_questions.innerHTML = '<div class="question"><p>'+ objJS[index].info[0] +'</p></div>'
-                                    + '<div class="question"><p>'+ objJS[index].info[1] +'</p></div>'
-                                    + '<div class="question"><p>'+ objJS[index].info[2] +'</p></div>'
-                                    + '<div class="question"><p>'+ objJS[index].info[3] +'</p></div>';
-
-            //Counter questions
-            countQuestions(count_span,objJS.length);
-            const question = document.querySelectorAll('.questions .question');
-            question.forEach(item=>{
-                item.setAttribute('onclick','questionSelected(this)');
-            })
+            //Dec fun boxQuiz
+            boxQuiz(obj_js,next_count);
+            // //Fun Selected questions
+            // questionSelected(index);
         }
     }
     myRequest.open("GET","assets/js/quizizy.json",true);
@@ -101,10 +79,40 @@ function showQuestions(index){
     // })
 }
 
+//BoxQuizez
+function boxQuiz(data,index){
+    //Select box quiz
+    const quiz_title         = document.querySelector('.box_quiz .title');
+    const quiz_questions     = document.querySelector('.box_quiz .questions');
+    quiz_title.innerHTML     = '<h2>'+ data[index].id +'. '+ data[index].question +'</h2>';
+    quiz_questions.innerHTML = '<div class="question"><p>'+ data[index].info[0] +'</p></div>'
+                            + '<div class="question"><p>'+ data[index].info[1] +'</p></div>'
+                            + '<div class="question"><p>'+ data[index].info[2] +'</p></div>'
+                            + '<div class="question"><p>'+ data[index].info[3] +'</p></div>'
+
+    //loop questions add add attribute onclick
+    const question = document.querySelectorAll('.questions .question');
+    question.forEach(item=>{
+        item.setAttribute('onclick','questionSelected(this,data)');
+    })
+
+    //Counter questions
+    countQuestions(index+1,data.length);
+    
+}
+
+//Selected questions
 function questionSelected(answer){
     let userAnswer = answer.textContent;
+    let correctAnswer = data[next_count].answers;
+    console.log(correctAnswer);
     console.log(userAnswer);
 }
 
-//showQuestions();
-
+// count question
+function countQuestions(n,lengthdata){
+    const count_question_step = document.querySelector(".quiz_footer .step");
+    const count_sum_question  = document.querySelector(".quiz_footer .all");
+    count_question_step.innerHTML = n;
+    count_sum_question.innerHTML  = lengthdata;
+};
