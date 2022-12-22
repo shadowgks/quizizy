dfd6622326d879262ee2edda68fc61260c3c9100
shadowgks <saad.moumou.coder@gmail.com>
2022-12-22 17:44:15 +0100

    ineed fix this

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,8 +41,7 @@ btn_continue[0].addEventListener('click',()=>{
 btn_continue[1].addEventListener('click',()=>{
     box_user.style.display = 'none';
     box_quiz.style.display = 'block';
-    showQuestion(0);
-    countQuestion(1);
+    showQuestions(0);
 });
 
 
@@ -53,46 +52,59 @@ btn_next.addEventListener('click',()=>{
     if(next_count < 9){  //ineed fix this
         next_count++;
         count_span++;
-        showQuestion(next_count);
-        countQuestion(count_span);
+        showQuestions(next_count);
     }else{
         console.log("good");
     }
 })
 // count question
-function countQuestion(n){
+function countQuestions(n,lengthdata){
     const count_question_step = document.querySelector(".quiz_footer .step");
     const count_sum_question  = document.querySelector(".quiz_footer .all");
     count_question_step.innerHTML = n;
-    count_sum_question.innerHTML  = 10;
+    count_sum_question.innerHTML  = lengthdata;
 };
-    
 
-function showQuestion(index){
-    // Get Info JSON
+function showQuestions(index){
+    // return new Promise((resolve,reject)=>{
+           // Get Info JSON
     let myRequest = new XMLHttpRequest();
     myRequest.onreadystatechange = function(){
         if(this.readyState === 4 && this.status === 200){
             //Convert To Object JS
             let objJS = JSON.parse(this.responseText);
-            //test
-            console.log(objJS[0].question);
+            console.log(objJS);
+            console.log(objJS[0].id );
+            // resolve(objJS)
+            // //testquiziz
+            // console.log(objJS[0].question);
 
             //Select box quiz
-            const quiz_title        = document.querySelector('.box_quiz .title');
-            const quiz_question     = document.querySelector('.box_quiz .questions');
-            quiz_title.innerHTML    = '<h2>'+ objJS[index].id +'. '+objJS[index].question +'</h2>';
-            quiz_question.innerHTML = '<div class="question"><p>'+ objJS[index].info[0] +'</p></div>'
+            const quiz_title         = document.querySelector('.box_quiz .title');
+            const quiz_questions     = document.querySelector('.box_quiz .questions');
+            quiz_title.innerHTML     = '<h2>'+ objJS[index].id +'. '+objJS[index].question +'</h2>';
+           quiz_questions.innerHTML = '<div class="question"><p>'+ objJS[index].info[0] +'</p></div>'
                                     + '<div class="question"><p>'+ objJS[index].info[1] +'</p></div>'
                                     + '<div class="question"><p>'+ objJS[index].info[2] +'</p></div>'
                                     + '<div class="question"><p>'+ objJS[index].info[3] +'</p></div>';
+
+            //Counter questions
+            countQuestions(count_span,objJS.length);
+            const question = document.querySelectorAll('.questions .question');
+            question.forEach(item=>{
+                item.setAttribute('onclick','questionSelected(this)');
+            })
         }
     }
     myRequest.open("GET","assets/js/quizizy.json",true);
     myRequest.send();   
+    // })
 }
 
+function questionSelected(answer){
+    let userAnswer = answer.textContent;
+    console.log(userAnswer);
+}
 
-
-
+//showQuestions();
 
